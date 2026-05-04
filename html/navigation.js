fda9e914a1e6c955bed19654d6a3fa6f0a1d4387
samulah <samu@SAMUN-POYTAKONE.localdomain>
2026-05-04 12:49:16 +0300

    löö

--- html/navigation.js
+++ html/navigation.js
@@ -12,7 +12,8 @@ class MainNavigation extends HTMLElement {
                 <a href="useampi-fakta.html">Useampi fakta</a> |
                 <a href="nimeamiskaytannot.html">Nimeämiskäytännöt</a> |
                 <a href="kirjallisuus-suositukset.html">Suositeltu kirjallisuus</a> |
-                <a href="apuohjelmat.html">Apuohjelmat</a>
+                <a href="apuohjelmat.html">Apuohjelmat</a> |
+                <a href="data-governance.html">Data Governance</a>
             </nav>
         `;
     }
