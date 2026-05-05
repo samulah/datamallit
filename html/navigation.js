eda2e28509fa6ca39c18cd11645abeb97856d5c0
samulah <samu@SAMUN-POYTAKONE.localdomain>
2026-05-05 08:41:32 +0300

    mmm...

--- html/navigation.js
+++ html/navigation.js
@@ -3,17 +3,8 @@ class MainNavigation extends HTMLElement {
         this.innerHTML = `
             <nav class="simple-nav">
                 <a href="index.html">Etusivu</a> |
-                <a href="data-vault.html">Data Vault</a> |
-                <a href="etl-elt.html">ETL/ELT</a> |
-                <a href="header-detail.html">Header-Detail</a> |
-                <a href="lumihiutalemalli.html">Lumihiutalemalli</a> |
-                <a href="medallion.html">Medallion</a> |
                 <a href="tahtimalli.html">Tähtimalli</a> |
-                <a href="useampi-fakta.html">Useampi fakta</a> |
-                <a href="nimeamiskaytannot.html">Nimeämiskäytännöt</a> |
-                <a href="kirjallisuus-suositukset.html">Suositeltu kirjallisuus</a> |
-                <a href="apuohjelmat.html">Apuohjelmat</a> |
-                <a href="data-governance.html">Data Governance</a>
+                <a href="nimeamiskaytannot.html">Nimeämiskäytännöt</a>
             </nav>
         `;
     }
